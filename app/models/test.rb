ff54715b05adea6bfdb351b63141053b50b3fbb1
elenachekhina <elenachekhina@yandex.ru>
2022-11-18 22:07:24 +0400

    Lesson 4: correct methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,14 +1,15 @@
 class Test < ApplicationRecord
   belongs_to :category
-  has_many :questions
   belongs_to :author, foreign_key: 'author_id', class_name: 'User'
-  has_many :tests_results
+
+  has_many :questions, dependent: :nullify
+  has_many :tests_results, dependent: :destroy
   has_many :users, through: :tests_results
   class << self
     def tests_of_category(category)
-      joins('JOIN categories on categories.id = tests.category_id')
-        .where('categories.title = ?', category).order(title: :desc)
-        .pluck(:title)
+      Category.where(title: category).joins(:tests)
+              .order(title: :desc)
+              .pluck(:title)
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
-  has_many :published_tests, foreign_key: 'author_id', class_name: 'Test'
-  has_many :tests_results
+  has_many :published_tests, foreign_key: 'author_id', class_name: 'Test', dependent: :nullify
+  has_many :tests_results, dependent: :destroy
   has_many :tests, through: :tests_results
   def tests_of_level(level)
-    TestsResult.where(user_id: self.id)
-               .joins('JOIN tests on tests.id = tests_results.test_id')
-               .where('tests.level = ?', level).select('tests.*')
+    tests.where(level: level)
   end
 end
