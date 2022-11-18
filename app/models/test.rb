41e3e210d2da365ed7a45710d2d1a5d10c6c06b7
elenachekhina <elenachekhina@yandex.ru>
2022-11-18 12:16:20 +0400

    Lesson 4: Associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  belongs_to :author, foreign_key: 'author_id', class_name: 'User'
+  has_many :tests_results
+  has_many :users, through: :tests_results
   class << self
     def tests_of_category(category)
       joins('JOIN categories on categories.id = tests.category_id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :published_tests, foreign_key: 'author_id', class_name: 'Test'
+  has_many :tests_results
+  has_many :tests, through: :tests_results
   def tests_of_level(level)
     TestsResult.where(user_id: self.id)
                .joins('JOIN tests on tests.id = tests_results.test_id')
