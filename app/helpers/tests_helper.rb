7a26b3b6a389c3c2a1cac7b3070ea92eab1b745e
elenachekhina <elenachekhina@yandex.ru>
2022-12-03 18:37:31 +0400

    Lesson 7: test levels

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,13 @@
 module TestsHelper
+  TEST_LEVELS = {
+    0 => :easy,
+    1 => :easy,
+    2 => :middle,
+    3 => :middle,
+    4 => :middle
+  }.freeze
+
+  def test_level(test)
+    TEST_LEVELS[test.level] || :hard
+  end
 end
